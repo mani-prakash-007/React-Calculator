ca0a9e75381640a920dce541abe483721b501dea
mani-prakash-007 <manip8072@gmail.com>
2024-08-01 10:29:28 +0530

    Add Some packagesc and Added Calculations

--- src/components/CalculatorBtn.jsx
+++ src/components/CalculatorBtn.jsx
@@ -1,31 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/CalculatorBtn.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { evaluate } from "mathjs";
 
 export const CalculatorBtn = () => {
+  const [input, setInput] = useState("0");
+
+  const handleInput = (userInput) => {
+    setInput((PrevUserInput) =>
+      PrevUserInput === "0"
+        ? userInput.toString()
+        : PrevUserInput + userInput.toString()
+    );
+  };
+  const handleClear = () => {
+    setInput("0");
+  };
+  const handleEval = () => {
+    const result = evaluate(input).toString();
+    if (result === "Infinity") {
+      setInput("Error");
+    } else {
+      setInput(result);
+    }
+  };
+  const handleClearOne = () => {
+    if (input === "Error") {
+      setInput("0");
+    } else if (input.length === 1) {
+      setInput("0");
+    } else {
+      const result = input.slice(0, -1);
+      setInput(result);
+    }
+  };
   return (
     <div className="buttons">
       <div>
-        <input type="text" placeholder="0" />
+        <input type="text" placeholder="0" value={input} readOnly />
         <div className="btn-grid">
-          <div className="button symbol1">AC</div>
-          <div className="button symbol1">C</div>
-          <div className="button symbol1">%</div>
-          <div className="button symbol">÷</div>
-          <div className="button number">7</div>
-          <div className="button number">8</div>
-          <div className="button number">9</div>
-          <div className="button symbol">x</div>
-          <div className="button number">4</div>
-          <div className="button number">5</div>
-          <div className="button number">6</div>
-          <div className="button symbol">-</div>
-          <div className="button number">3</div>
-          <div className="button number">2</div>
-          <div className="button number">1</div>
-          <div className="button symbol">+</div>
-          <div className="button number double">0</div>
-          <div className="button number">.</div>
-          <div className="button symbol">=</div>
+          <div className="button symbol1" onClick={() => handleClear()}>
+            AC
+          </div>
+          <div className="button symbol1" onClick={() => handleClearOne()}>
+            <IoMdArrowRoundBack />
+          </div>
+          <div className="button symbol1" onClick={() => handleInput("%")}>
+            %
+          </div>
+          <div className="button symbol" onClick={() => handleInput("/")}>
+            ÷
+          </div>
+          <div className="button number" onClick={() => handleInput(7)}>
+            7
+          </div>
+          <div className="button number" onClick={() => handleInput(8)}>
+            8
+          </div>
+          <div className="button number" onClick={() => handleInput(9)}>
+            9
+          </div>
+          <div className="button symbol" onClick={() => handleInput("*")}>
+            x
+          </div>
+          <div className="button number" onClick={() => handleInput(4)}>
+            4
+          </div>
+          <div className="button number" onClick={() => handleInput(5)}>
+            5
+          </div>
+          <div className="button number" onClick={() => handleInput(6)}>
+            6
+          </div>
+          <div className="button symbol" onClick={() => handleInput("-")}>
+            -
+          </div>
+          <div className="button number" onClick={() => handleInput(3)}>
+            3
+          </div>
+          <div className="button number" onClick={() => handleInput(2)}>
+            2
+          </div>
+          <div className="button number" onClick={() => handleInput(1)}>
+            1
+          </div>
+          <div className="button symbol" onClick={() => handleInput("+")}>
+            +
+          </div>
+          <div className="button number double" onClick={() => handleInput(0)}>
+            0
+          </div>
+          <div className="button number" onClick={() => handleInput(0)}>
+            .
+          </div>
+          <div className="button symbol" onClick={() => handleEval()}>
+            =
+          </div>
         </div>
       </div>
     </div>
