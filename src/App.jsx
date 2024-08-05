d5e82daf5410ceda8f7dd72dc908d7336567a64f
mani-prakash-007 <manip8072@gmail.com>
2024-08-05 19:44:31 +0530

    Added History Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
       <Header />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,11 @@ import { evaluate } from "mathjs";
 
 export const Main = () => {
   const [input, setInput] = useState("0");
+  const [history, setHistory] = useState([]);
+  const [btnValue, setBtnValue] = useState("History");
+  const [calculatorDisplayValue, setCalculatorDisplayValue] = useState("Block");
+  const [historyDisplayValue, setHistoryDisplayValue] = useState("none");
+  const [contentAlignment, setContentAlignment] = useState("center");
   const handleInput = (userInput) => {
     setInput((PrevUserInput) =>
       PrevUserInput === "0"
@@ -16,11 +21,13 @@ export const Main = () => {
     setInput("0");
   };
   const handleEval = () => {
+    const expression = `${input} = ${evaluate(input).toString()}`;
     const result = evaluate(input).toString();
     if (result === "Infinity") {
       setInput("Error");
     } else {
       setInput(result);
+      setHistory((prevHistory) => [expression, ...prevHistory]);
     }
   };
   const handleClearOne = () => {
@@ -80,70 +87,112 @@ export const Main = () => {
       document.removeEventListener("keydown", handleKeyEvents);
     };
   }, [input]);
+
+  const showHistory = () => {
+    if (btnValue === "History") {
+      setBtnValue("Calculator");
+      setHistoryDisplayValue("block");
+      setCalculatorDisplayValue("none");
+      setContentAlignment("flex-start");
+    }
+    if (btnValue === "Calculator") {
+      setBtnValue("History");
+      setHistoryDisplayValue("none");
+      setCalculatorDisplayValue("block");
+      setContentAlignment("center");
+    }
+  };
+
+  const clearHistory = () => {
+    setHistory([]);
+  };
   return (
-    <div className="buttons">
-      <div>
-        <input type="text" placeholder="0" value={input} readOnly disabled />
-        <div className="btn-grid">
-          <div className="button symbol1" onClick={() => handleClear()}>
-            AC
+    <>
+      <div className="buttons" style={{ justifyContent: contentAlignment }}>
+        <div style={{ display: calculatorDisplayValue }}>
+          <input type="text" placeholder="0" value={input} readOnly disabled />
+          <div className="btn-grid">
+            <div className="button symbol1" onClick={() => handleClear()}>
+              AC
+            </div>
+            <div className="button symbol1" onClick={() => handleClearOne()}>
+              <FaDeleteLeft />
+            </div>
+            <div className="button symbol1" onClick={() => handleInput("%")}>
+              %
+            </div>
+            <div className="button symbol" onClick={() => handleInput("/")}>
+              ÷
+            </div>
+            <div className="button number" onClick={() => handleInput(7)}>
+              7
+            </div>
+            <div className="button number" onClick={() => handleInput(8)}>
+              8
+            </div>
+            <div className="button number" onClick={() => handleInput(9)}>
+              9
+            </div>
+            <div className="button symbol" onClick={() => handleInput("*")}>
+              x
+            </div>
+            <div className="button number" onClick={() => handleInput(4)}>
+              4
+            </div>
+            <div className="button number" onClick={() => handleInput(5)}>
+              5
+            </div>
+            <div className="button number" onClick={() => handleInput(6)}>
+              6
+            </div>
+            <div className="button symbol" onClick={() => handleInput("-")}>
+              -
+            </div>
+            <div className="button number" onClick={() => handleInput(1)}>
+              1
+            </div>
+            <div className="button number" onClick={() => handleInput(2)}>
+              2
+            </div>
+            <div className="button number" onClick={() => handleInput(3)}>
+              3
+            </div>
+            <div className="button symbol" onClick={() => handleInput("+")}>
+              +
+            </div>
+            <div
+              className="button number double"
+              onClick={() => handleInput(0)}
+            >
+              0
+            </div>
+            <div className="button number" onClick={() => handleInput(0)}>
+              .
+            </div>
+            <div className="button symbol" onClick={() => handleEval()}>
+              =
+            </div>
           </div>
-          <div className="button symbol1" onClick={() => handleClearOne()}>
-            <FaDeleteLeft />
+        </div>
+        <div style={{ display: historyDisplayValue }} className="history">
+          <div className="historyHeader">
+            <div className="historyHeading">History</div>
+            <div className="clearBtn" onClick={clearHistory}>
+              Clear
+            </div>
           </div>
-          <div className="button symbol1" onClick={() => handleInput("%")}>
-            %
-          </div>
-          <div className="button symbol" onClick={() => handleInput("/")}>
-            ÷
-          </div>
-          <div className="button number" onClick={() => handleInput(7)}>
-            7
-          </div>
-          <div className="button number" onClick={() => handleInput(8)}>
-            8
-          </div>
-          <div className="button number" onClick={() => handleInput(9)}>
-            9
-          </div>
-          <div className="button symbol" onClick={() => handleInput("*")}>
-            x
-          </div>
-          <div className="button number" onClick={() => handleInput(4)}>
-            4
-          </div>
-          <div className="button number" onClick={() => handleInput(5)}>
-            5
-          </div>
-          <div className="button number" onClick={() => handleInput(6)}>
-            6
-          </div>
-          <div className="button symbol" onClick={() => handleInput("-")}>
-            -
-          </div>
-          <div className="button number" onClick={() => handleInput(3)}>
-            3
-          </div>
-          <div className="button number" onClick={() => handleInput(2)}>
-            2
-          </div>
-          <div className="button number" onClick={() => handleInput(1)}>
-            1
-          </div>
-          <div className="button symbol" onClick={() => handleInput("+")}>
-            +
-          </div>
-          <div className="button number double" onClick={() => handleInput(0)}>
-            0
-          </div>
-          <div className="button number" onClick={() => handleInput(0)}>
-            .
-          </div>
-          <div className="button symbol" onClick={() => handleEval()}>
-            =
+          <div className="historyList">
+            {history.length === 0 ? (
+              <p className="list">No Records</p>
+            ) : (
+              history.map((record) => <p className="list">{record}</p>)
+            )}
           </div>
         </div>
       </div>
-    </div>
+      <div className="historybutton">
+        <button onClick={showHistory}>{btnValue}</button>
+      </div>
+    </>
   );
 };
